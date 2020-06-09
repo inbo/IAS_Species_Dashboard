1366873a72953ee1109e634e8f15b1d5e5ece361
SanderDevisscher <sander.devisscher@inbo.be>
2020-06-09 18:13:53 +0200

    get shinyapp working
    #3

--- R-scripts/deploy app.R
+++ R-scripts/deploy app.R
@@ -1,8 +1,11 @@
 library(rmarkdown)
 
-output_dir <- "./output"
-output_file <- "IAS_Species_Dashboard"
+rmarkdown::shiny_prerendered_clean
 
 library(rsconnect)
 rsconnect::deployApp("./R-scripts/all_species_dashboard.Rmd", appFiles = "./data/", upload = TRUE, forceUpdate = TRUE)
 
+# output_dir <- "./output"
+# output_file <- "IAS_Species_Dashboard"
+# 
+# render("./R-scripts/all_species_dashboard.Rmd", output_dir = output_dir, output_file = output_file, knit_root_dir = getwd(), runtime = "shiny")
